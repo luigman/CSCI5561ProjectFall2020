c57e131b51a5941ce5186b548d7d9c7b35f8e713
Luis Guzman <luigman@gmail.com>
2020-12-03 15:59:35 -0600

    Progress on extracting hand-object pairs. Can currently detect if an object is present in the last 20 frames.

--- hand_object_detector/hand_object_processing/iou.py
+++ hand_object_detector/hand_object_processing/iou.py
@@ -3,6 +3,7 @@ import os
 import time
 import pycocotools.mask as mask_util
 from detectron2.utils.colormap import random_color
+import random, string
 
 IOU_THRESHOLD = 0.2
 
@@ -47,41 +48,110 @@ def check_boxes(bbox1,bbox2):
 parent_dir = os.path.abspath(os.path.join(__file__, os.pardir, os.pardir))
 #doh_path = os.path.join(parent_dir, "images_det/")
 mask_rcnn_path = os.path.join(parent_dir, "maskrcnn_det/")
+subsample = 5
 
 def processData():
     for root, directories, filenames in os.walk(mask_rcnn_path): 
         video_visualizer = VideoVisualizer() #new visulizer instance for each directory (video)
+        objIDlist = []
+        detectedList = []
         for filename in sorted(filenames):
-            if (filename.endswith(".npz")):
+            if filename.endswith(".npz"):# and int(filename[6:-4])<500):
+                frameNum = int(filename[6:-4])
+                frameInd = frameNum // subsample
                 path = os.path.join(root,filename)
-                print(path)
                 pathList = path.split('/')
                 pathList[pathList.index("maskrcnn_det")] = "images_det"
                 doh_path = "/".join(pathList[:-1]) + "/"
 
-                data = np.load(path)
-                maskrcnn_boxes = data['boxes']
-                maskrcnn_labels = data['classes']
-                label_list = data['label'].tolist()
-                maskrcnn_boxes = maskrcnn_boxes.reshape(-1,4)
-                doh_data = np.load(doh_path + filename.split('.')[0] + '.npz', allow_pickle=True)
-                doh_boxes = doh_data['objects']
-                doh_hands = doh_data['hands']
+                if os.path.isfile(doh_path + filename.split('.')[0] + '.npz'):
+                    print(path)
+                    data = np.load(path)
+                    maskrcnn_boxes = data['boxes']
+                    maskrcnn_labels = data['classes']
+                    label_list = data['label'].tolist()
+                    maskrcnn_boxes = maskrcnn_boxes.reshape(-1,4)
+                    doh_data = np.load(doh_path + filename.split('.')[0] + '.npz', allow_pickle=True)
+                    doh_boxes = doh_data['objects']
+                    doh_hands = doh_data['hands']
 
-                #imageStabilization(doh_hands, maskrcnn_boxes, os.path.join(doh_path + filename.split('.')[0] + "stab"))
-                #findContactLabel(doh_boxes, doh_hands, maskrcnn_boxes, os.path.join(doh_path + filename + ".txt"), label_list, maskrcnn_labels)
-                detected = [
-                    _DetectedInstance(maskrcnn_labels[i], maskrcnn_boxes[i], mask_rle=None, color=None, ttl=8)
-                    for i in range(len(maskrcnn_labels))
-                ]
+                    detected = [
+                        _DetectedInstance(maskrcnn_labels[i], maskrcnn_boxes[i], mask_rle=None, color=None, ttl=8, objID=None)
+                        for i in range(len(maskrcnn_labels))
+                    ]
+                    
+                    objIDs = video_visualizer.assign_ids(detected)
+                    objIDlist.append(objIDs)
+                    detectedList.append(detected)
+
+                    hand2obj = imageStabilization(doh_hands, maskrcnn_boxes, os.path.join(doh_path + filename.split('.')[0] + "stab"), objIDs)
+                    #findContactLabel(doh_boxes, doh_hands, maskrcnn_boxes, os.path.join(doh_path + filename + ".txt"), label_list, maskrcnn_labels, objIDs)
                 
-                video_visualizer.assign_colors(detected)
+                series_refresh_rate = 10 #Defines a new series every x frames
+                series_length = 20
+                if len(objIDlist) > series_length and frameInd % series_refresh_rate == 0:
+                    if objIDlist[-1] is not None:
+                        print(np.all(objIDlist[-1] == [d.objID for d in detectedList[-1]]))
+                        for objID in objIDlist[-1]: #loop through objects in most recent frame
+                            #Do the last 20 frames have objID in them?
+                            checkArray = np.zeros(series_length)
+                            series = np.zeros(series_length)
+                            for i in range(series_length): 
+                                if objIDlist[-i-1] is None:
+                                    checkArray[i] = False
+                                else:
+                                    checkArray[i] = objID in objIDlist[-i-1]
+                                    #series[i] = 
+                            #print(checkArray)
+                            if np.all(checkArray):
+                                print("true")
 
 
+
+
+def loadData():
+    #OLD VERSION... REMOVE ONCE ASSIGN_COLORS IS WORKING
+    parent_dir = os.path.abspath(os.path.join(__file__, os.pardir, os.pardir))
+    hand2obj_path = os.path.join(parent_dir, "hand_object_detector/images_det/")
+    data = []
+    for root, directories, filenames in os.walk(hand2obj_path): 
+        vidName = root.split('/')[-1]
+        print(vidName)
+        videoData = np.zeros((len(filenames)//4, 2))
+        for filename in filenames:
+            if (filename.endswith("stab.npy")):
+                print("  "+filename)
+                index = int(filename[6:-8])//10
+                stab = np.load(os.path.join(root,filename), allow_pickle=True)
+                if stab.size > 1:
+                    videoData[index,:] = stab[0,0,:]#.reshape(-1,2)
+
+        detected = [
+            _DetectedInstance(classes[i], boxes[i], mask_rle=None, color=None, ttl=8)
+            for i in range(num_instances)
+        ]
+        colors = self._assign_colors(detected)
+
+        if (videoData.size != 0):
+            plt.plot(videoData[:10,0],videoData[:10,1])
+            plt.plot(videoData[100:110,0],videoData[100:110,1])
+            plt.show()
+            #if len(dirname)==7:
+            #    print(dirname)
+            #if (filename.endswith("stab.npy")):
+            #    print(root.split('/')[-1])
+            #    print(dirname)
+            #
+            #    np.load(os.path.join(root,filename)).reshape(-1,2)
+
+    for root, dirs, files in os.walk(hand2obj_path):
+        dirs.sort()
+        for d in dirs:
+            print(os.path.join(root, d))
 
                 
 
-def imageStabilization(doh_hands, maskrcnn_boxes, save_path):
+def imageStabilization(doh_hands, maskrcnn_boxes, save_path, objIDs):
     if doh_hands.size > 1:
         hand2obj = np.zeros((doh_hands.shape[0], maskrcnn_boxes.shape[0], 2))
         for j,doh_box in enumerate(doh_hands):
@@ -90,9 +160,9 @@ def imageStabilization(doh_hands, maskrcnn_boxes, save_path):
     else:
         hand2obj = None
     np.save(save_path, hand2obj)
-    return
+    return hand2obj
 
-def findContactLabel(doh_boxes, doh_hands, maskrcnn_boxes, save_path, label_list, labels):
+def findContactLabel(doh_boxes, doh_hands, maskrcnn_boxes, save_path, label_list, labels, objIDs):
     """
     Cross-checks IoU for doh_boxes and maskrcnn_boxes, and saves a text file with the matching label, if found
     Also prints the location of detected hands to the text file
@@ -101,27 +171,31 @@ def findContactLabel(doh_boxes, doh_hands, maskrcnn_boxes, save_path, label_list
     for j,doh_box in enumerate(doh_boxes):
         max_iou = -1
         bestBox = 0
+        bestID = 0
         for i,maskrcnn_box in enumerate(maskrcnn_boxes):
         
             
             val = bb_intersection_over_union(doh_box,maskrcnn_box)
             if (val > max_iou):
                 bestLabel = str(label_list[labels[i]])
+                bestID = objIDs[i]
                 bestBox = maskrcnn_box
                 max_iou = val
 
         if (doh_boxes.shape[0] == 0):
             print("No_contact", file = outputFile, end=',')
-            print("NaN,NaN,NaN", file = outputFile, end=',')
+            print("NaN,NaN,NaN,NaN", file = outputFile, end=',')
         elif (max_iou > IOU_THRESHOLD):
             cx, cy = getCentroid(bestBox)
             print(bestLabel, file = outputFile, end=',')
+            print(bestID, file = outputFile, end=',')
             print(cx, file = outputFile, end=',')
             print(cy, file = outputFile, end=',')
             print(str(max_iou), file = outputFile)
         else:
             cx, cy = getCentroid(doh_box)
             print("unknown", file = outputFile, end=',')
+            print("NaN", file = outputFile, end=',')
             print(cx, file = outputFile, end=',')
             print(cy, file = outputFile, end=',')
             print("NaN", file = outputFile)
@@ -139,7 +213,7 @@ class VideoVisualizer:
     def __init__(self):
         self._old_instances = []
             
-    def assign_colors(self, instances):
+    def assign_ids(self, instances):
             """
             Modified from detectron2/utils/video_visualizer.py
 
@@ -186,6 +260,8 @@ class VideoVisualizer:
                     newidx = matched_new_per_old[idx]
                     if instances[newidx].color is None:
                         instances[newidx].color = inst.color
+                    if instances[newidx].objID is None:
+                        instances[newidx].objID = inst.objID
                         continue
                 # If an old instance does not match any new instances,
                 # keep it for the next frame in case it is just missed by the detector
@@ -197,8 +273,11 @@ class VideoVisualizer:
             for inst in instances:
                 if inst.color is None:
                     inst.color = random_color(rgb=True, maximum=1)
+                if inst.objID is None:
+                    #Assign random 32-bit hex key
+                    inst.objID = ''.join(random.choices(string.ascii_letters + string.digits, k=32))
             self._old_instances = instances[:] + extra_instances
-            return [d.color for d in instances]
+            return [d.objID for d in instances]
 
 class _DetectedInstance:
     """
@@ -216,13 +295,14 @@ class _DetectedInstance:
             the instance color can be transferred to objects in the next two frames.
     """
 
-    __slots__ = ["label", "bbox", "mask_rle", "color", "ttl"]
+    __slots__ = ["label", "bbox", "mask_rle", "color", "ttl", "objID"]
 
-    def __init__(self, label, bbox, mask_rle, color, ttl):
+    def __init__(self, label, bbox, mask_rle, color, ttl, objID):
         self.label = label
         self.bbox = bbox
         self.mask_rle = mask_rle
         self.color = color
         self.ttl = ttl
+        self.objID = objID
 
 #checkIoU()
