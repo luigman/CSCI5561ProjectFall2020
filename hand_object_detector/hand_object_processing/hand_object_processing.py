2bb9e3d2c7e935f0a65d9558af3b4c7f80dd7fdc
Luis Guzman <luigman@gmail.com>
2020-12-04 17:14:17 -0600

    Modified hand_object_demo to skip already processed frames

--- hand_object_detector/hand_object_processing/hand_object_processing.py
+++ hand_object_detector/hand_object_processing/hand_object_processing.py
@@ -5,10 +5,10 @@ sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 import hand_object_demo
 
 print("Running detectron")
-detectron2demo.run()
+#detectron2demo.run()
 os.chdir('..') #Fixes some directory dependencies of hand_object_demo
 print("Running hand-object detection")
-#hand_object_demo.run()
+hand_object_demo.run()
 os.chdir('./hand_object_processing')
 print("Running IOU check")
 iou.processData()
