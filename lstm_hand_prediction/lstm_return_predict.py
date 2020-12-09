25de67d3a0ae1863d7e4fdab557f50d58e951d09
Kasai2020 <isaac.kasahara@gmail.com>
2020-12-09 01:57:13 -0600

    Update lstm_return_predict.py

--- lstm_hand_prediction/lstm_return_predict.py
+++ lstm_hand_prediction/lstm_return_predict.py
@@ -1,6 +1,6 @@
 import tensorflow as tf
 #Fixes "Fail to find the dnn implementation." error on my computer
-physical_devices = tf.config.list_physical_devices('GPU')
+#physical_devices = tf.config.list_physical_devices('GPU')
 #tf.config.experimental.set_memory_growth(physical_devices[0], enable=True)
 import numpy as np
 import pandas as pd
@@ -9,6 +9,28 @@ import random
 import math
 import os
 
+# def loadData():
+#     parent_dir = os.path.abspath(os.path.join(__file__, os.pardir, os.pardir))
+#     series_path = os.path.join(parent_dir, "hand_object_detector/maskrcnn_det/")
+#     series = []
+#     for root, directories, filenames in os.walk(series_path):
+#         #vidName = root.split('/')[-1]
+#         folders = ['P94', 'P95', 'P96', 'P97', 'P98', 'P99']
+#         for folder in folders:
+#             if folder in root: #only load "nice" data
+#                 for filename in filenames:
+#                     if (filename.endswith("stab.npy")):
+#                         #print("  "+filename)
+#                         seriesVid = np.load(os.path.join(root,filename), allow_pickle=True)
+#                         series.append(seriesVid)
+#     #Remove contact label for now
+#     for i in range(0,len(series)):
+#         a,b,c = np.shape(series[i])
+#         if c == 3:
+#             series[i] = np.delete(series[i],2,2)
+#     data = np.vstack(series)
+#     print(np.shape(data))
+#     return data
 
 def normalize(data_points, max):
     start_point = data_points[0]
@@ -29,6 +51,7 @@ def undo_normalize(norm_points, start_point, max):
 #takes in previous 8 points to calculate prediction
 def lstm_return_predict(prev_8, num_steps_pred):
     assert len(prev_8) == 8
+    # print(prev_8)
     #max values from training
     max = [853.01153564, 707.01364136]
     norm_data, start_point = normalize(prev_8, max)
@@ -47,13 +70,17 @@ def lstm_return_predict(prev_8, num_steps_pred):
     prediction = np.transpose(prediction)
 
     final_pred = undo_normalize(prediction, start_point, max)
-    print(final_pred)
+    #print(final_pred)
 
-    plot_final = np.transpose(final_pred)
-    plot_prev = np.transpose(prev_8)
-    plt.clf()
-    plt.plot(plot_final[0],plot_final[1], '--')
-    plt.plot(plot_prev[0],plot_prev[1])
-    plt.savefig('prediction.png')
-    #plt.savefig('prediction{0}.png'.format(number))
+    # plot_final = np.transpose(final_pred)
+    # plot_prev = np.transpose(prev_8)
+    # plt.clf()
+    # plt.plot(plot_final[0],plot_final[1], '--')
+    # plt.plot(plot_prev[0],plot_prev[1])
+    # plt.savefig('prediction.png')
     return final_pred
+
+# if __name__ == '__main__':
+#     data = loadData()
+#     prediction = lstm_return_predict(data[0][:8], 15)
+#     print(prediction)
