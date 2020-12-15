83a830120a87e4cd94d12522be65b0c88b2d2750
Luis Guzman <luigman@gmail.com>
2020-12-14 18:05:18 -0600

    Multiple Objects working

--- run_on_video/run_on_video.py
+++ run_on_video/run_on_video.py
@@ -8,7 +8,7 @@ from lstm_return_predict import lstm_return_predict
 sys.path.append('../hand_object_detector/hand_object_processing')
 from iou import VideoVisualizer, _DetectedInstance, getCentroid
 
-vidName = 'P96_101'
+vidName = 'P95_102'
 
 seriesList = np.load(vidName+'stab.npy', allow_pickle=True)
 meta = np.load(vidName+'meta.npz', allow_pickle=True)
@@ -26,6 +26,7 @@ errors = []
 for filename in sorted(os.listdir(vidName)):
     showPlot = False
     predictedSeries = []
+    centroids_location = [] 
     print(filename)
     frameNum = int(filename.split('.')[0][6:])
     if frameNum < 5:
@@ -54,29 +55,37 @@ for filename in sorted(os.listdir(vidName)):
         showPlot = True
         pastSeries = np.flipud(seriesList[i][5:13]) #flip because LSTM is predicting backwards
         futureSeries = seriesList[i][:5]
-        predictedSeries = lstm_return_predict(pastSeries[:,:2], 5, modelLSTM)
+        predictedSeries.append(lstm_return_predict(pastSeries[:,:2], 5, modelLSTM))
         frameLabel = 1 in futureSeries[:,2]
 
         objx, objy = getCentroid(objBBsPrev[i])
+        centroids_location.append((objBBsPrev[i]))  # make sure to get the probabilities in the same order as the boxes appear 
 
         cv.rectangle(img,(objBBsPrev[i][0], objBBsPrev[i][1]),(objBBsPrev[i][2], objBBsPrev[i][3]), (0,255,0),2)
         plt.plot(objx-pastSeries[:,0], objy-pastSeries[:,1], label='Past Series')
         plt.plot(objx-futureSeries[:,0], objy-futureSeries[:,1], label='Future Series')
-        plt.plot(objx-predictedSeries[:,0], objy-predictedSeries[:,1], label='Predicted Series')
+        plt.plot(objx-predictedSeries[-1][:,0], objy-predictedSeries[-1][:,1], label='Predicted Series')
 
+    contact_prob = []
     if len(predictedSeries) > 0:
+        predictedSeries = np.array(predictedSeries)
         print("Has prediction")
         #MLP code goes here
         def rowNorm(X):
             return np.sum(np.abs(X)**2,axis=-1)**(1./2)
-        x1 = rowNorm(predictedSeries)
-        m = 1106.50695399817    # Max value of training data after taking norm. Need to divide by this to get accurate results from the model
-        x1 = (x1 / m).reshape((-1,5))
-        y_pred = modelMLP.predict(x1)
-        contact_prob = y_pred[0,1]
-        frameLabelPred = contact_prob > 0.5
-        errors.append(frameLabel == frameLabelPred)
-        plt.text(0,0,"Contact Probability: " + str(contact_prob))
+        for series in predictedSeries:
+            x1 = rowNorm(series)
+            m = 1106.50695399817    # Max value of training data after taking norm. Need to divide by this to get accurate results from the model
+            x1 = (x1 / m).reshape((-1,5))
+            y_pred = modelMLP.predict(x1)
+            contact_prob.append(y_pred[0,1])
+
+            frameLabelPred = contact_prob[-1] > 0.5
+            errors.append(frameLabel == frameLabelPred)
+
+        centroids_location = np.array(centroids_location) 
+        for i in range(centroids_location.shape[0]): 
+            plt.text(centroids_location[i][0],centroids_location[i][1],"P: " + f'{contact_prob[i]:.2f}',fontsize=10) 
 
 
     if showPlot:
@@ -85,6 +94,7 @@ for filename in sorted(os.listdir(vidName)):
         #if pastHand.size>1:
             #plt.plot(pastHand[:,0], pastHand[:,1], label="Past Motion")
         plt.legend()
+        plt.savefig("saved_imgs/" + filename) 
         plt.show()
 
 print("Accuracy:", np.sum(errors)/len(errors))
